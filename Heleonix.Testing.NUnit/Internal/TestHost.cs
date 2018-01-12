0fa017e1a8ea66f99a33f9a6033cbefc0aaea6eb
Hennadii Lutsyshyn <Hennadiilu@gmail.com>
2018-01-12 23:56:13 +0200

    Initial commit.

---
+++ Heleonix.Testing.NUnit/Internal/TestHost.cs
@@ -0,0 +1,123 @@
+﻿// <copyright file="TestHost.cs" company="Heleonix - Hennadii Lutsyshyn">
+// Copyright (c) 2018-present Heleonix - Hennadii Lutsyshyn. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
+namespace Heleonix.Testing.NUnit.Internal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+    using global::NUnit.Framework;
+
+    /// <summary>
+    /// Represents a base test host to be implemented by implementations of different tests patterns.
+    /// </summary>
+    internal abstract class TestHost
+    {
+        private readonly SpecNode root = new SpecNode(SpecNodeType.Root, null, null);
+        private readonly Stack<SpecNode> specExecutionStack = new Stack<SpecNode>();
+
+        /// <summary>
+        /// Gets the current instance of the <see cref="TestHost"/>.
+        /// </summary>
+        /// <value>
+        /// The current instance of the <see cref="TestHost"/>.
+        /// </value>
+        public static TestHost Current => TestProperties.GetTestHost(TestContext.CurrentContext.Test.Properties);
+
+        /// <summary>
+        /// Gets the spec structure rules.
+        /// </summary>
+        /// <value>
+        /// The spec structure rules.
+        /// </value>
+        protected abstract IDictionary<SpecNodeType, SpecStructureRule> SpecStructureRules { get; }
+
+        /// <summary>
+        /// Removes the specified node.
+        /// </summary>
+        /// <param name="node">The node.</param>
+        public static void Remove(SpecNode node)
+        {
+            node.Parent.Remove(node);
+        }
+
+        /// <summary>
+        /// Adds the specified node.
+        /// </summary>
+        /// <param name="node">The node.</param>
+        public void Add(SpecNode node)
+        {
+            this.ValidateAdding(node);
+
+            var parent = this.specExecutionStack.Count > 0 ? this.specExecutionStack.Peek() : this.root;
+
+            parent.Add(node);
+        }
+
+        /// <summary>
+        /// Executes the specified node.
+        /// </summary>
+        /// <param name="node">The node.</param>
+        public void Execute(SpecNode node)
+        {
+            var properties = TestContext.CurrentContext.Test.Properties;
+
+            if (!TestProperties.IsOutputWritten(properties))
+            {
+                foreach (var prop in TestProperties.GetOutput(properties))
+                {
+                    TestContext.WriteLine(prop);
+                }
+
+                TestProperties.SetIsOutputWritten(properties);
+            }
+
+            if (!string.IsNullOrEmpty(node.Description))
+            {
+                TestContext.WriteLine($"{new string(' ', node.NestingLevel * 4)}{node.Description}");
+            }
+
+            this.specExecutionStack.Push(node);
+
+            try
+            {
+                node.Action();
+            }
+            finally
+            {
+                this.specExecutionStack.Pop();
+            }
+        }
+
+        private void ValidateAdding(SpecNode child)
+        {
+            if (!this.SpecStructureRules.ContainsKey(child.Type))
+            {
+                throw new InvalidOperationException($"Invalid spec: '{child.Type}'");
+            }
+
+            var rule = this.SpecStructureRules[child.Type];
+
+            if (rule.SpecExecutionStackRule != null
+                && !Regex.IsMatch(string.Join(",", this.specExecutionStack), rule.SpecExecutionStackRule))
+            {
+                throw new InvalidOperationException($"Invalid test structure: cannot place the '{child.Type}' " +
+                    $"into the execution stack '{string.Join("->", this.specExecutionStack.Reverse())}'");
+            }
+
+            if (rule.PredecessorsRule != null)
+            {
+                var parent = this.specExecutionStack.Count > 0 ? this.specExecutionStack.Peek() : this.root;
+
+                if (!Regex.IsMatch(string.Join(",", parent.Children.Reverse()), rule.PredecessorsRule))
+                {
+                    throw new InvalidOperationException($"Invalid test structure: cannot place the '{child.Type}' " +
+                    $"after the '{string.Join("->", parent.Children)}'");
+                }
+            }
+        }
+    }
+}
---
+++ Heleonix.Testing.NUnit/Internal/TestProperties.cs
@@ -0,0 +1,77 @@
+﻿// <copyright file="TestProperties.cs" company="Heleonix - Hennadii Lutsyshyn">
+// Copyright (c) 2018-present Heleonix - Hennadii Lutsyshyn. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the repository root for full license information.
+// </copyright>
+
+namespace Heleonix.Testing.NUnit.Internal
+{
+    using System;
+    using System.Linq;
+    using global::NUnit.Framework.Interfaces;
+
+    /// <summary>
+    /// Represents the properties of the test and methods to work with the <see cref="IPropertyBag"/>.
+    /// </summary>
+    internal static class TestProperties
+    {
+        private const string TestHostName = nameof(TestHost);
+
+        private static readonly string Prefix = typeof(TestProperties).Namespace + ".";
+
+        private static readonly string IsOutputWrittenName = PropertyName(nameof(IsOutputWritten));
+
+        private static readonly string OutputPrefix = Prefix + "Output.";
+
+        /// <summary>
+        /// Gets the properties to be written to the output.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <returns>The properties to be written to the output.</returns>
+        public static object[] GetOutput(IPropertyBag properties) =>
+            properties.Keys.Where(key => key.StartsWith(OutputPrefix, StringComparison.Ordinal))
+                .SelectMany(key => properties[key].OfType<object>()).ToArray();
+
+        /// <summary>
+        /// Gets the test host.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <returns>The test host.</returns>
+        public static TestHost GetTestHost(IPropertyBag properties) => properties[TestHostName][0] as TestHost;
+
+        /// <summary>
+        /// Determines whether output is written.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <returns>
+        ///   <c>true</c> if output has been written; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsOutputWritten(IPropertyBag properties) =>
+            properties[IsOutputWrittenName].Contains(true);
+
+        /// <summary>
+        /// Builds a name of an output property.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns>A name of an output property.</returns>
+        public static string OutputPropertyName(string name) => OutputPrefix + name;
+
+        /// <summary>
+        /// Sets <c>true</c> if output has been written; otherwise <c>false</c>.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        public static void SetIsOutputWritten(IPropertyBag properties) =>
+            properties.Set(IsOutputWrittenName, true);
+
+        /// <summary>
+        /// Sets the test host.
+        /// </summary>
+        /// <param name="properties">The properties.</param>
+        /// <param name="node">The node.</param>
+        public static void SetTestHost(IPropertyBag properties, TestHost node)
+        {
+            properties.Set(TestHostName, node);
+        }
+
+        private static string PropertyName(string name) => Prefix + name;
+    }
+}
