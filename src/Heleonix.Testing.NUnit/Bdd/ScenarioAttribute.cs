8529dd94b5e26b667d2f7ad2970c363ff8f2fc2a
Hennadii Lutsyshyn <hennadii.lutsyshyn@gmail.com>
2023-05-30 22:03:00 +0300

    feat: Writing fixture and test names to the output in BDD tests.

--- src/Heleonix.Testing.NUnit/Bdd/ScenarioAttribute.cs
+++ src/Heleonix.Testing.NUnit/Bdd/ScenarioAttribute.cs
@@ -16,7 +16,7 @@ namespace Heleonix.Testing.NUnit.Bdd
     /// </summary>
     /// <seealso cref="TestAttribute" />
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
-    public sealed class ScenarioAttribute : TestAttribute
+    public sealed class ScenarioAttribute : TestAttribute, IApplyToContext
     {
         /// <summary>
         /// Gets or sets the name of the scenario.
@@ -41,6 +41,17 @@ namespace Heleonix.Testing.NUnit.Bdd
         }
 
         /// <summary>
+        /// Gets the properties.
+        /// </summary>
+        /// <value>
+        /// The properties.
+        /// </value>
+        protected override IDictionary<string, object> Properties => new Dictionary<string, object>
+        {
+            { TestPropertiesHelper.OutputPropertyName(nameof(this.TestName)), this.TestName },
+        };
+
+        /// <summary>
         /// Build a TestMethod from the provided MethodInfo.
         /// </summary>
         /// <param name="method">The method to be used as a test.</param>
@@ -52,9 +63,27 @@ namespace Heleonix.Testing.NUnit.Bdd
         {
             var testMethod = base.BuildFrom(method, suite);
 
-            TestPropertiesHelper.SetTestHost(testMethod.Properties, new BddTestHost(-1));
+            TestPropertiesHelper.SetTestHost(testMethod.Properties, new BddTestHost(1));
 
             return testMethod;
+        }
+
+        /// <inheritdoc/>
+        public void ApplyToContext(TestExecutionContext context)
+        {
+            var fixtureOutputProperties = TestPropertiesHelper.GetOutput(context.CurrentTest.Parent.Properties);
+
+            foreach (var prop in fixtureOutputProperties)
+            {
+                context.OutWriter.WriteLine(prop);
+            }
+
+            var testOutputProperties = TestPropertiesHelper.GetOutput(context.CurrentTest.Properties);
+
+            foreach (var prop in testOutputProperties)
+            {
+                context.OutWriter.WriteLine($"    {prop}");
+            }
         }
     }
 }
